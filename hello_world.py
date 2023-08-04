92474d1841595cb910aae76ce255519920c493ab
sinking-in-sky-blue <119094401+sinking-in-sky-blue@users.noreply.github.com>
2023-08-04 15:16:12 +0800

    Calculator initial version
    只支援+-*/、2位數的計算機-初版

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,18 @@
 # 2023/08/04 14:42
-print("Hello world!")
+print("Calculator:")
+operator = input("請輸入+-*/:")
+numA = int(input)
+numB = int(input())
+result = 0
+if(operator == "+"):
+    result = numA + numB
+elif(operator == "-"):
+    result = numA - numB
+elif(operator == "*"):
+    result = numA * numB
+elif(operator == "/"):
+    result = numA / numB
+else:
+    print("您輸入錯誤")
+
+print(f" {numA} {operator} {numB} = {result}")
